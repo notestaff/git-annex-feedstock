5b265ddc63d414e10dfdf6aafb0b71ea42ecbbd3
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2018-12-14 16:48:01 -0500

    added back magicmime

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,7 +20,7 @@ STACK_OPTS="--local-bin-path ${PREFIX}/bin --extra-include-dirs ${PREFIX}/includ
 mkdir -p ${PREFIX}/bin
 
 stack ${STACK_OPTS} setup
-stack ${STACK_OPTS} install --ghc-options "-optl-L${PREFIX}/lib -optl-Wl,-rpath,${PREFIX}/lib"
+stack ${STACK_OPTS} install --ghc-options "-optl-L${PREFIX}/lib -optl-Wl,-rpath,${PREFIX}/lib" --flag git-annex:magicmime
 
 ln -s ${PREFIX}/bin/git-annex ${PREFIX}/bin/git-annex-shell
 rm -rf ${PACKAGE_HOME}
