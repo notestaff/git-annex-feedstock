89b70af53db9874d248bc63d1b37714f282a89cf
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-03-14 16:33:04 +0000

    add to LDFLAGS etc rather than replacing; reformat whitespace

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,47 +2,47 @@
 
 set -e -o pipefail -x
 
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="  -L${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PTH}"
+export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+export LDFLAGS=" -L${PREFIX}/lib ${LDFLAGS} "
+export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS} "
 
 export GMP_INCLUDE_DIRS=$PREFIX/include
 export GMP_LIB_DIRS=$PREFIX/lib
 
-echo "#!/bin/bash" > $CC-mine
-echo "set -e -o pipefail -x " >> $CC-mine
-echo "$CC -I$PREFIX/include   -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CC-mine
-chmod u+x $CC-mine
-export CC=$CC-mine
+echo "#!/bin/bash" > $CC-shim
+echo "set -e -o pipefail -x " >> $CC-shim
+echo "$CC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CC-shim
+chmod u+x $CC-shim
+export CC=$CC-shim
 
-echo "#!/bin/bash" > $CXX-mine
-echo "set -e -o pipefail -x " >> $CXX-mine
-echo "$CXX -I$PREFIX/include   -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CXX-mine
-chmod u+x $CXX-mine
-export CXX=$CXX-mine
+echo "#!/bin/bash" > $CXX-shim
+echo "set -e -o pipefail -x " >> $CXX-shim
+echo "$CXX -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CXX-shim
+chmod u+x $CXX-shim
+export CXX=$CXX-shim
 
-echo "#!/bin/bash" > $GCC-mine
-echo "set -e -o pipefail -x " >> $GCC-mine
-echo "$GCC -I$PREFIX/include   -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GCC-mine
-chmod u+x $GCC-mine
-export GCC=$GCC-mine
+echo "#!/bin/bash" > $GCC-shim
+echo "set -e -o pipefail -x " >> $GCC-shim
+echo "$GCC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GCC-shim
+chmod u+x $GCC-shim
+export GCC=$GCC-shim
 
-echo "#!/bin/bash" > $GXX-mine
-echo "set -e -o pipefail -x " >> $GXX-mine
-echo "$GXX -I$PREFIX/include   -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GXX-mine
-chmod u+x $GXX-mine
-export GXX=$GXX-mine
+echo "#!/bin/bash" > $GXX-shim
+echo "set -e -o pipefail -x " >> $GXX-shim
+echo "$GXX -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GXX-shim
+chmod u+x $GXX-shim
+export GXX=$GXX-shim
 
-echo "#!/bin/bash" > $LD-mine
-echo "set -e -o pipefail -x " >> $LD-mine
-echo "$LD   -L$PREFIX/lib  \"\$@\"" >> $LD-mine
-chmod u+x $LD-mine
-export LD=$LD-mine
+echo "#!/bin/bash" > $LD-shim
+echo "set -e -o pipefail -x " >> $LD-shim
+echo "$LD -L$PREFIX/lib \"\$@\"" >> $LD-shim
+chmod u+x $LD-shim
+export LD=$LD-shim
 
 echo "#!/bin/bash" > ${LD}.gold
 echo "set -e -o pipefail -x " >> ${LD}.gold
-echo "$LD_GOLD   -L$PREFIX/lib  \"\$@\"" >> ${LD}.gold
+echo "$LD_GOLD -L$PREFIX/lib \"\$@\"" >> ${LD}.gold
 chmod u+x ${LD}.gold
 export LD_GOLD=${LD}.gold
 
@@ -52,13 +52,13 @@ ln -s /lib64/libpthread.so.0 ${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.
 mkdir -p ~/.stack
 echo "extra-include-dirs:"  > ~/.stack/config.yaml
 echo "- ${PREFIX}/include" >> ~/.stack/config.yaml
-echo "extra-lib-dirs:" >> ~/.stack/config.yaml
-echo "- ${PREFIX}/lib" >> ~/.stack/config.yaml
-echo "ghc-options:" >> ~/.stack/config.yaml
+echo "extra-lib-dirs:"     >> ~/.stack/config.yaml
+echo "- ${PREFIX}/lib"     >> ~/.stack/config.yaml
+echo "ghc-options:"        >> ~/.stack/config.yaml
 echo "  \"\$everything\": -optc-I${PREFIX}/include -optl-L${PREFIX}/lib" >> ~/.stack/config.yaml
 echo "apply-ghc-options: everything" >> ~/.stack/config.yaml
 
 stack setup
 stack update
-stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --ghc-options " -optc-I${PREFIX}/include -optl-L${PREFIX}/lib "  --local-bin-path ${PREFIX}/bin
+stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --ghc-options " -optc-I${PREFIX}/include -optl-L${PREFIX}/lib " --local-bin-path ${PREFIX}/bin
 ln -s ${PREFIX}/bin/git-annex ${PREFIX}/bin/git-annex-shell
