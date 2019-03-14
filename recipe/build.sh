b216b0663702c4d3bf412b5c6c7ffad492a14e69
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-03-14 13:21:53 +0000

    cleaned up recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -46,35 +46,19 @@ echo "$LD_GOLD  -L/lib64 -L$PREFIX/lib  \"\$@\"" >> ${LD}.gold
 chmod u+x ${LD}.gold
 export LD_GOLD=${LD}.gold
 
-rm ${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib/libpthread.so
-ln -s /lib64/libpthread.so.0 ${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib/libpthread.so
+rm ${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so
+ln -s /lib64/libpthread.so.0 ${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so
 
-#git clone git://git-annex.branchable.com/ git-annex
-#pushd git-annex
-#cp $RECIPE_DIR/stack-lts-9.9.yaml stack.yaml
 mkdir -p ~/.stack
 echo "extra-include-dirs:"  > ~/.stack/config.yaml
 echo "- ${PREFIX}/include" >> ~/.stack/config.yaml
 echo "extra-lib-dirs:" >> ~/.stack/config.yaml
 echo "- ${PREFIX}/lib" >> ~/.stack/config.yaml
-#echo "compiler: ghc-8.2.2" >> ~/.stack/config.yaml
 echo "ghc-options:" >> ~/.stack/config.yaml
 echo "  \"\$everything\": -optc-I${PREFIX}/include -optl-L${PREFIX}/lib" >> ~/.stack/config.yaml
 echo "apply-ghc-options: everything" >> ~/.stack/config.yaml
 
-cp $RECIPE_DIR/stack.yaml .
-stack --resolver=lts-11.22 setup
-stack --resolver=lts-11.22 update
-stack --resolver=lts-11.22 install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --ghc-options " -optc-I${PREFIX}/include -optl-L${PREFIX}/lib "  --local-bin-path ${PREFIX}/bin
+stack setup
+stack update
+stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --ghc-options " -optc-I${PREFIX}/include -optl-L${PREFIX}/lib "  --local-bin-path ${PREFIX}/bin
 ln -s ${PREFIX}/bin/git-annex ${PREFIX}/bin/git-annex-shell
-which git-annex
-git-annex version
-#git-annex test
-#stack runghc git-annex version
-#stack runghc git-annex test
-#mv ${PREFIX}/bin/RNAlien ${PREFIX}/bin/RNAlien-bin
-#echo -e "#!/bin/bash\nexport SYSTEM_CERTIFICATE_PATH=/usr/local/ssl/cacert.pem\n${PREFIX}/bin/RNAlien-bin \"\$@\"\n" > ${PREFIX}/bin/RNAlien
-#chmod 755 ${PREFIX}/bin/RNAlien
-#cleanup
-#rm -r .stack-work
-
