338a5b9fd08f1a2723b33bb2470145cadb8af59e
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-03-18 18:56:56 +0000

    WIP try osx only

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,59 +10,59 @@ export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS} "
 export GMP_INCLUDE_DIRS=$PREFIX/include
 export GMP_LIB_DIRS=$PREFIX/lib
 
-echo "#!/bin/bash" > $CC-shim
-echo "set -e -o pipefail -x " >> $CC-shim
-echo "$CC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CC-shim
-chmod u+x $CC-shim
-export CC=$CC-shim
+# echo "#!/bin/bash" > $CC-shim
+# echo "set -e -o pipefail -x " >> $CC-shim
+# echo "$CC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CC-shim
+# chmod u+x $CC-shim
+# export CC=$CC-shim
 
-echo "#!/bin/bash" > $CXX-shim
-echo "set -e -o pipefail -x " >> $CXX-shim
-echo "$CXX -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CXX-shim
-chmod u+x $CXX-shim
-export CXX=$CXX-shim
+# echo "#!/bin/bash" > $CXX-shim
+# echo "set -e -o pipefail -x " >> $CXX-shim
+# echo "$CXX -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CXX-shim
+# chmod u+x $CXX-shim
+# export CXX=$CXX-shim
 
 
-if [ -z ${GCC+x} ];
-then
-    echo "GCC is unset"
-else
-    echo "#!/bin/bash" > $GCC-shim
-    echo "set -e -o pipefail -x " >> $GCC-shim
-    echo "$GCC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GCC-shim
-    chmod u+x $GCC-shim
-    export GCC=$GCC-shim
-fi
+# if [ -z ${GCC+x} ];
+# then
+#     echo "GCC is unset"
+# else
+#     echo "#!/bin/bash" > $GCC-shim
+#     echo "set -e -o pipefail -x " >> $GCC-shim
+#     echo "$GCC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GCC-shim
+#     chmod u+x $GCC-shim
+#     export GCC=$GCC-shim
+# fi
 
-if [ -z ${GXX+x} ];
-then
-    echo "GXX is unset"
-else
-    echo "#!/bin/bash" > $GXX-shim
-    echo "set -e -o pipefail -x " >> $GXX-shim
-    echo "$GXX -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GXX-shim
-    chmod u+x $GXX-shim
-    export GXX=$GXX-shim
-fi
+# if [ -z ${GXX+x} ];
+# then
+#     echo "GXX is unset"
+# else
+#     echo "#!/bin/bash" > $GXX-shim
+#     echo "set -e -o pipefail -x " >> $GXX-shim
+#     echo "$GXX -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GXX-shim
+#     chmod u+x $GXX-shim
+#     export GXX=$GXX-shim
+# fi
 
-echo "#!/bin/bash" > $LD-shim
-echo "set -e -o pipefail -x " >> $LD-shim
-echo "$LD -L$PREFIX/lib \"\$@\"" >> $LD-shim
-chmod u+x $LD-shim
-export LD=$LD-shim
+# echo "#!/bin/bash" > $LD-shim
+# echo "set -e -o pipefail -x " >> $LD-shim
+# echo "$LD -L$PREFIX/lib \"\$@\"" >> $LD-shim
+# chmod u+x $LD-shim
+# export LD=$LD-shim
 
-echo "#!/bin/bash" > ${LD}.gold
-echo "set -e -o pipefail -x " >> ${LD}.gold
-echo "$LD_GOLD -L$PREFIX/lib \"\$@\"" >> ${LD}.gold
-chmod u+x ${LD}.gold
-export LD_GOLD=${LD}.gold
+# echo "#!/bin/bash" > ${LD}.gold
+# echo "set -e -o pipefail -x " >> ${LD}.gold
+# echo "$LD_GOLD -L$PREFIX/lib \"\$@\"" >> ${LD}.gold
+# chmod u+x ${LD}.gold
+# export LD_GOLD=${LD}.gold
 
-LIBPTHREAD_SO="${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so"
-if [ -f "${LIBPTHREAD_SO}" ];
-then
-    rm ${LIBPTHREAD_SO}
-    ln -s /lib64/libpthread.so.0 ${LIBPTHREAD_SO}
-fi
+# LIBPTHREAD_SO="${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so"
+# if [ -f "${LIBPTHREAD_SO}" ];
+# then
+#     rm ${LIBPTHREAD_SO}
+#     ln -s /lib64/libpthread.so.0 ${LIBPTHREAD_SO}
+# fi
 
 mkdir -p ~/.stack
 echo "extra-include-dirs:"  > ~/.stack/config.yaml
