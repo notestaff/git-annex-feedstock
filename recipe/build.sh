6fa497fe262ce5c2277593170d0bb9b1c180de2e
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-03-22 18:58:42 -0400

    update to 7.032219; add dbus; move STACK_ROOT into $SRC_DIR; clean up build.sh; remove some unused dependencies (#45)
    * move STACK_ROOT into $SRC_DIR; clean up build.sh; remove some unused dependencies
    
    * added missing part to move STACK_ROOT iside $SRC_DIR
    
    * dropped patching
    
    * changed to new git-annex version; adde dbus
    
    * added missing pkg for dbus

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,22 @@
 
 set -e -o pipefail -x
 
-export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PTH}"
+#######################################################################################################
+# Set up build environment
+#######################################################################################################
+
+mkdir -p $PREFIX/bin $BUILD_PREFIX/bin $PREFIX/lib $BUILD_PREFIX/lib $PREFIX/share $BUILD_PREFIX/share
+export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
 export LDFLAGS=" -L${PREFIX}/lib ${LDFLAGS} "
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS} "
 
 export GMP_INCLUDE_DIRS=$PREFIX/include
 export GMP_LIB_DIRS=$PREFIX/lib
+
+#
+# Install shim scripts to ensure that certain flags are always passed to the compiler/linker
+#
 
 echo "#!/bin/bash" > $CC-shim
 echo "set -e -o pipefail -x " >> $CC-shim
@@ -46,19 +55,28 @@ echo "$LD_GOLD -L$PREFIX/lib \"\$@\"" >> ${LD}.gold
 chmod u+x ${LD}.gold
 export LD_GOLD=${LD}.gold
 
-rm ${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so
-ln -s /lib64/libpthread.so.0 ${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so
+HOST_LIBPTHREAD="${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so"
+rm ${HOST_LIBPTHREAD}
+ln -s /lib64/libpthread.so.0 ${HOST_LIBPTHREAD}
 
-mkdir -p ~/.stack
-echo "extra-include-dirs:"  > ~/.stack/config.yaml
-echo "- ${PREFIX}/include" >> ~/.stack/config.yaml
-echo "extra-lib-dirs:"     >> ~/.stack/config.yaml
-echo "- ${PREFIX}/lib"     >> ~/.stack/config.yaml
-echo "ghc-options:"        >> ~/.stack/config.yaml
-echo "  \"\$everything\": -optc-I${PREFIX}/include -optl-L${PREFIX}/lib" >> ~/.stack/config.yaml
-echo "apply-ghc-options: everything" >> ~/.stack/config.yaml
+#######################################################################################################
+# Build git-annex
+#######################################################################################################
 
-stack setup
-stack update
-stack install --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --ghc-options " -optc-I${PREFIX}/include -optl-L${PREFIX}/lib " --local-bin-path ${PREFIX}/bin
+export STACK_ROOT=${SRC_DIR}/stack_root
+mkdir -p $STACK_ROOT
+( 
+    echo "extra-include-dirs:"
+    echo "- ${PREFIX}/include"
+    echo "extra-lib-dirs:"
+    echo "- ${PREFIX}/lib"
+    echo "ghc-options:"
+    echo "  \"\$everything\": -optc-I${PREFIX}/include -optl-L${PREFIX}/lib"
+    echo "apply-ghc-options: everything"
+) > "${STACK_ROOT}/config.yaml"
+
+cp ${RECIPE_DIR}/git_annex_custom_snapshot.yaml .
+stack setup --resolver git_annex_custom_snapshot.yaml
+stack update --resolver git_annex_custom_snapshot.yaml
+stack install --resolver git_annex_custom_snapshot.yaml --extra-include-dirs ${PREFIX}/include --extra-lib-dirs ${PREFIX}/lib --ghc-options " -optc-I${PREFIX}/include -optl-L${PREFIX}/lib " --local-bin-path ${PREFIX}/bin --flag git-annex:magicmime --flag git-annex:dbus
 ln -s ${PREFIX}/bin/git-annex ${PREFIX}/bin/git-annex-shell
