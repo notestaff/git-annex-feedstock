6caf1420b70ab7e6a1b1e5358221957e173d9ec5
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-11-08 10:49:20 -0500

    update to 7.20191106 (#67)
    * updated to 7.20191106
    
    * MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.6.2, and conda-forge-pinning 2019.11.01
    
    * added more clear warnings about incompatible changes
    
    * added libiconv as a run dependency because of a conda warning
    
    * fixed warning message

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -7,11 +7,17 @@ function msg {
     echo $1 >> $MSGS 2>&1
 }
 
-msg
-msg '============= This git-annex version introduces breaking changes  ==========================================='
-msg 'In short: the behavior of "git add" may be affected depending on your configuration.                         '
-msg "Please see https://git-annex.branchable.com/news/version_${PKG_VERSION}/ for details of what changed.        "
-msg '============================================================================================================='
-
-
-
+msg "============================================================================================================="
+msg "============= git-annex recently introduced changes that may break old workflows ============================"
+msg "============================================================================================================="
+msg "                                                                                                             "
+msg "In short: the behavior of \"git add\" may be affected depending on your configuration.                       "
+msg "                                                                                                             "
+msg "If annex.largefiles is set then git add will add large files to the annex, not to git, unless you also set   "
+msg "annex.gitaddtoannex=false .  Files added to the annex by git add will be in unlocked state.                  "
+msg "v5 repos will be auto-upgraded to v7, unless you set annex.autoupgraderepository=false.                      "
+msg "                                                                                                             "
+msg "Please see https://hackage.haskell.org/package/git-annex-${PKG_VERSION}/changelog and                        "
+msg "https://git-annex.branchable.com/news/version_${PKG_VERSION}/ for details of what changed.                   "
+msg "                                                                                                             "
+msg "============================================================================================================="
