1434fd52c58ed59a279e7b15a3079248cb757cc2
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-03-03 16:35:55 -0500

    fix build script for standalone/not

--- recipe/build_standaloneFalse.sh
+++ recipe/build_standaloneFalse.sh
@@ -2,21 +2,6 @@
 
 set -e -o pipefail -x
 
-if [[ $PKG_BUILD_STRING == *"standalone"* ]]; then
-    BINARY_HOME=$PREFIX/bin
-    PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
-
-    mkdir -p $BINARY_HOME
-    mkdir -p $PACKAGE_HOME
-
-    cp ${SRC_DIR}/git-annex-standalone-amd64.tar.gz_ ${PACKAGE_HOME}/git-annex-standalone-amd64.tar.gz
-    tar xvfz -C ${PACKAGE_HOME} ${PACKAGE_HOME}/git-annex-standalone-amd64.tar.gz
-    ln -s ${PACKAGE_HOME}/git-annex.linux/git-annex $PREFIX/bin/
-    ln -s ${PACKAGE_HOME}/git-annex.linux/git-annex-shell $PREFIX/bin/
-    exit 0
-fi
-
-
 #######################################################################################################
 # Set up build environment
 #######################################################################################################
