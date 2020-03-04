3f9b26446038920c3c18da30289f89aa6fa36efe
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-03-04 00:41:03 -0500

    add standalone build (#84)
    Added a nodep build variant that has no dependencies.

---
+++ recipe/build_nodepTrue.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e -o pipefail -x
+
+BINARY_HOME=$PREFIX/bin
+PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+
+mkdir -p $BINARY_HOME
+mkdir -p $PACKAGE_HOME
+
+mv * ${PACKAGE_HOME}/
+ln -s ${PACKAGE_HOME}/git-annex $PREFIX/bin/
+ln -s ${PACKAGE_HOME}/git-annex-shell $PREFIX/bin/
