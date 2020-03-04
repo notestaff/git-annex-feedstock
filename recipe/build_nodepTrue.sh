a8afdcacfbd23307ee136897101a1a63a69dbbec
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-03-03 22:25:31 -0500

    fix build script

--- recipe/build_nodepTrue.sh
+++ recipe/build_nodepTrue.sh
@@ -9,7 +9,5 @@ mkdir -p $BINARY_HOME
 mkdir -p $PACKAGE_HOME
 
 mv * ${PACKAGE_HOME}/
-cp ${SRC_DIR}/git-annex-standalone-amd64.tar.gz_ ${PACKAGE_HOME}/git-annex-standalone-amd64.tar.gz
-tar -C ${PACKAGE_HOME} -xvzf  ${PACKAGE_HOME}/git-annex-standalone-amd64.tar.gz
 ln -s ${PACKAGE_HOME}/git-annex.linux/git-annex $PREFIX/bin/
 ln -s ${PACKAGE_HOME}/git-annex.linux/git-annex-shell $PREFIX/bin/
