903b2f1004403520696c823466d4aa0197313952
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-10-14 16:40:55 -0400

    enabled ghc/gcc compiler optimizations; added testing of downstream packages. (#104)
    * added testing of downstream packages annexremote and datalad
    
    * added findutils to build req
    
    * whitespace change to force rebuild
    
    * clarify message printed when standalone version is installed
    
    * whitespace change to force rebuild
    
    * disable testing of downstream package datalad for the nodep version for now
    
    * add whitespace to force rebuild
    
    * try enabling compiler optimization
    
    * update build number
    
    * added whitespace to force rebuild
    
    * fixed whitespace
    
    * update datalad downstream test version

--- recipe/build_nodepFalse.sh
+++ recipe/build_nodepFalse.sh
@@ -27,25 +27,25 @@ export GMP_LIB_DIRS=$PREFIX/lib
 
 echo "#!/bin/bash" > $CC-shim
 echo "set -e -o pipefail -x " >> $CC-shim
-echo "$CC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $CC-shim
+echo "$CC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC -O2 \"\$@\"" >> $CC-shim
 chmod u+x $CC-shim
 export CC=$CC-shim
 
 echo "#!/bin/bash" > $GCC-shim
 echo "set -e -o pipefail -x " >> $GCC-shim
-echo "$GCC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC \"\$@\"" >> $GCC-shim
+echo "$GCC -I$PREFIX/include -L$PREFIX/lib -pthread -fPIC -O2 \"\$@\"" >> $GCC-shim
 chmod u+x $GCC-shim
 export GCC=$GCC-shim
 
 echo "#!/bin/bash" > $LD-shim
 echo "set -e -o pipefail -x " >> $LD-shim
-echo "$LD -L$PREFIX/lib \"\$@\"" >> $LD-shim
+echo "$LD -L$PREFIX/lib -O2 \"\$@\"" >> $LD-shim
 chmod u+x $LD-shim
 export LD=$LD-shim
 
 echo "#!/bin/bash" > ${LD}.gold
 echo "set -e -o pipefail -x " >> ${LD}.gold
-echo "$LD_GOLD -L$PREFIX/lib \"\$@\"" >> ${LD}.gold
+echo "$LD_GOLD -L$PREFIX/lib -O2 \"\$@\"" >> ${LD}.gold
 chmod u+x ${LD}.gold
 export LD_GOLD=${LD}.gold
 
@@ -95,7 +95,7 @@ pushd ${SRC_DIR}/ghc_src
 
 touch mk/build.mk
 #echo "HADDOCK_DOCS = NO" >> mk/build.mk
-echo "BuildFlavour = quick" >> mk/build.mk
+echo "BuildFlavour = perf" >> mk/build.mk
 echo "libraries/integer-gmp_CONFIGURE_OPTS += --configure-option=--with-gmp-includes=$PREFIX/include" >> mk/build.mk
 echo "libraries/integer-gmp_CONFIGURE_OPTS += --configure-option=--with-gmp-libraries=$PREFIX/lib" >> mk/build.mk
 echo "STRIP_CMD = $STRIP" >> build.mk
@@ -189,3 +189,4 @@ ln -s ${PREFIX}/bin/git-annex ${PREFIX}/bin/git-annex-shell
 echo "========CALLING STACK INSTALL==========="
 #make install BUILDER=stack PREFIX=${PREFIX}
 popd
+
